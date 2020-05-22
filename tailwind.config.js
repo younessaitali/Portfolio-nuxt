8138c694d16ac94c51d8c0fccbe52982df61f19d
younessaitali <younes.aitali95@gmail.com>
2020-05-22 15:11:31 +0000

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        color: {
+          bg: "var(--bg)",
+          bg_nav: "var(--bg-nav)",
+          bg_footer: "var(--bg-footer)",
+          text: "var(--text)",
+          secondary_text: "var(--secondary-text)",
+          border_color: "var(--border-color)",
+          bg_solar: "var(--bg-solar)"
+        }
+      }
+    }
+  },
   variants: {},
   plugins: []
-}
+};
